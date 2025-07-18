9dda6fbdd496c0b0fa31642699f677ef346a0b0d
amir haim <amirbiron@gmail.com>
2025-07-18 17:14:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import os
 import logging
 from datetime import datetime, timedelta
@@ -13,7 +16,7 @@ from telegram.ext import (
 )
 from telegram.constants import ParseMode
 
-from database_manager import Database
+from database.database_manager import Database
 from config import Config
 
 # הגדרת לוגים
